cc6cdf1484f46383571591dfd2a04509c3fd44cb
Daniel Alejandro Lopez Munera <danielalejandrolopez97@gmail.com>
2020-08-11 11:10:09 +0200

    EqualSumChecker

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,13 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int firstValue, int secondValue, int thirdValue){
+
+        if((firstValue + secondValue) == thirdValue){
+            return true;
+        }
+        return false;
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("The sum of the first and second values are equal to the third? " + EqualSumChecker.hasEqualSum(1,-1,0));
+
+
+    }
+}
